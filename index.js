064a5773502a9bd66026f9260af923a3c7e5323c
dasoib <dasojib49@gmail.comm>
2023-05-21 10:27:44 +0600

    alltoys show done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const {MongoClient,ServerApiVersion} = require('mongodb');
+const {MongoClient,ServerApiVersion, ObjectId} = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_PASS)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d2bgkkq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +25,25 @@ async function run() {
           try {
                     // Connect the client to the server	(optional starting in v4.7)
                     await client.connect();
+
+                    const toysCollection = client.db('toyCars').collection('allToys');
+
+                    app.get('/toys',async(req,res)=>{
+                              const cursor = toysCollection.find();
+                              const result = await cursor.toArray();
+                              res.send(result)
+                    })
+
+                    app.get('/toys/:id',async(req,res)=>{
+                              const id = req.params.id;
+                              const query = {_id: new ObjectId(id)}
+                              const result = await toysCollection.findOne(query)
+                              res.send(result)
+                    })
+
+
+
+
                     // Send a ping to confirm a successful connection
                     await client.db("admin").command({
                               ping: 1
